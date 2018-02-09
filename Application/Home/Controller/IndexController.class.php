514f22cbddb0873724676ee237ffdf703c6ced80
宋宇飞 <925796005@qq.com>
2018-02-09 23:19:58 +0800

    前台导航全部完善。

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -19,42 +19,56 @@ class IndexController extends HomeController {
 	//系统首页
     public function index(){
 
-        $category = D('Category')->getTree();
-        $lists    = D('Document')->lists(null);
+//        $category = D('Category')->getTree();
+//        $lists    = D('Document')->lists(null);
+//
+//        $this->assign('category',$category);//栏目
+//        $this->assign('lists',$lists);//列表
+//        $this->assign('page',D('Document')->page);//分页
 
-        $this->assign('category',$category);//栏目
-        $this->assign('lists',$lists);//列表
-        $this->assign('page',D('Document')->page);//分页
 
-                 
+        $this->assign('focus',"首页");
         $this->display();
     }
 
     public function rescue(){
+
+        $this->assign('focus',"一呼百应");//列表
         $this->display();
     }
 
     public function instructions(){
+        $this->assign('focus',"使用钦家");
         $this->display();
     }
 
     public function introduction(){
+        $this->assign('focus',"关于钦家");
+        $this->assign('column',"品牌故事");
         $this->display();
     }
 
     public function technology(){
+        $this->assign('focus',"关于钦家");
+        $this->assign('column',"领先技术");
         $this->display();
     }
 
     public function patent(){
+        $this->assign('focus',"关于钦家");
+        $this->assign('column',"独家专利");
         $this->display();
     }
 
     public function join_us(){
+        $this->assign('focus',"关于钦家");
+        $this->assign('column',"加入我们");
         $this->display();
     }
 
     public function center(){
+        $this->assign('focus',"关于钦家");
+        $this->assign('column',"服务与帮助");
         $this->display();
     }
 
