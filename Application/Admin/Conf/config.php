7bebd3b9d29cefbaa721307ef93b249931fe7781
宋宇飞 <925796005@qq.com>
2018-02-10 22:42:18 +0800

    删除小bug修复 ，图片压缩处理

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -98,8 +98,8 @@ return array(
     'VAR_SESSION_ID' => 'session_id',	//修复uploadify插件无法传递session_id的bug
 
     /* 后台错误页面模板 */
-    'TMPL_ACTION_ERROR'     =>  'Public/error.html', // 默认错误跳转对应的模板文件
-    'TMPL_ACTION_SUCCESS'   =>  'Public/success.html', // 默认成功跳转对应的模板文件
-    'TMPL_EXCEPTION_FILE'   =>  'Public/exception.html',// 异常页面的模板文件
+    'TMPL_ACTION_ERROR'     =>  'Public/error', // 默认错误跳转对应的模板文件
+    'TMPL_ACTION_SUCCESS'   =>  'Public/success', // 默认成功跳转对应的模板文件
+    'TMPL_EXCEPTION_FILE'   =>  'Public/exception',// 异常页面的模板文件
 
 );
